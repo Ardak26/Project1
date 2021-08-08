353a1f5671211ba3a5f52ec929d9c769722e0740
Ardak <example@gmal.com>
2021-08-08 18:24:54 +0600

    first comit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+// alert ("Hello");
+// const result = confirm("Are you here?");
+// console.log (result);
+// const answer = +prompt("Вам есть есть 18?", "18");
+// console.log(answer+5);
+const answers = [];
+// answers[0] = prompt(" Как ваше имя?", " ");
+// answers[1] = prompt(" Как ваше Фамилия?", " ");
+// answers[2] = prompt(" Сколько вам лет?", " ");
+
+// console.log(typeof (answers));
+
+// const category = "toys";
+// console.log(`https: //someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+console.log("arr"+"- object");
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(2*4 == '8');
+
+const isChecked = true, 
+        isClose = false;
+console.log(isChecked || isClose);
